367cb4fb01ab60a919c9507bd846824c3712c46e
Siavash Kavousi <siavash.kavousi@outlook.com>
2015-10-14 02:52:17 +0330

    Fixes view pager and drawer

--- app/src/main/java/com/aspire/dubsmash/siavash/FragmentViewPager.java
+++ app/src/main/java/com/aspire/dubsmash/siavash/FragmentViewPager.java
@@ -2,19 +2,18 @@ package com.aspire.dubsmash.siavash;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aspire.dubsmash.R;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v13.FragmentPagerItems;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -25,39 +24,28 @@ import butterknife.OnClick;
  */
 public class FragmentViewPager extends Fragment {
 
-    @Bind(R.id.toolbar) Toolbar mToolbar;
+    @Bind(R.id.view_pager_tab) SmartTabLayout mViewPagerTab;
     @Bind(R.id.view_pager) ViewPager mViewPager;
-    private AdapterPager mAdapterPager;
 
     @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_pager, container, false);
         ButterKnife.bind(this, view);
-
-        // Set up toolbar
-        if (getActivity() instanceof AppCompatActivity) {
-            ((AppCompatActivity) getActivity()).setSupportActionBar(mToolbar);
-        }
-
         // Set up view pager
-        List<Fragment> fragments = getFragments();
-        mAdapterPager = new AdapterPager(getFragmentManager(), fragments);
-        mViewPager.setAdapter(mAdapterPager);
+        setUpViewPager();
+
         return view;
     }
 
-    @OnClick(R.id.toolbar_hamburger) void onDrawerClick() {
-        if (getActivity() instanceof ActivityMain){
-            ((ActivityMain) getActivity()).mDrawerLayout.openDrawer(GravityCompat.END);
-        }
+    private void setUpViewPager() {
+        mViewPager.setAdapter(setUpViewPagerAdapter());
+        mViewPagerTab.setViewPager(mViewPager);
     }
 
-    private List<Fragment> getFragments() {
-        FragmentSounds fragmentSounds = new FragmentSounds();
-//        FragmentVideos fragmentVideos = new FragmentVideos();
-
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(fragmentSounds);
-//        fragments.add(fragmentVideos);
-        return fragments;
+    @NonNull private FragmentPagerItemAdapter setUpViewPagerAdapter() {
+        return new FragmentPagerItemAdapter(
+                getFragmentManager(), FragmentPagerItems.with(getActivity())
+                .add("صدا ها", FragmentSounds.class)
+                .add("داب ها", FragmentVideos.class)
+                .create());
     }
 }
